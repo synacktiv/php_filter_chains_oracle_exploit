30dde07cdd7fa820984c56a0d7041edd5a940f25
remsio-syn <remi.matasse+github@synacktiv.com>
2024-02-27 16:12:15 +0100

    Adding support of JSON payloads

---
+++ filters_chain_oracle/core/utils.py
@@ -0,0 +1,8 @@
+def merge_dicts(dict1, dict2):
+    merged = dict1.copy()
+    for key, value in dict2.items():
+        if key in merged and isinstance(merged[key], dict) and isinstance(value, dict):
+            merged[key] = merge_dicts(merged[key], value)
+        else:
+            merged[key] = value
+    return merged
